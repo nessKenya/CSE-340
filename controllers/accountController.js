1e925d1ae98acc8e131ad4f5b6cbea98bdd7ceb4
nessKenya <nelson.muchonji@gmail.com>
2025-01-29 12:23:17 +0300

    Data Validation: Client side and Server Side

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -23,6 +23,7 @@ async function buildRegister(req, res, next) {
   res.render("account/register", {
     title: "Register",
     nav,
+    errors: null,
   })
 }
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 /* ***********************************************************
 * Deliver Login View
@@ -20,7 +21,13 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 /* ***********************************************************
 * Enable the Registration
+* Process the registration data
 * ***********************************************************/
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router
