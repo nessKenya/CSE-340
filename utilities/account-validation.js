9098c216fc9ceb99d11c13212b2456ed49bc439e
nessKenya <nelson.muchonji@gmail.com>
2025-01-30 14:00:27 +0300

    Email Check and Form Stickiness and Password Hashing

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,6 +1,8 @@
 const utilities = require(".")
   const { body, validationResult } = require("express-validator")
   const validate = {}
+  const accountModel = require("../models/account-model")
+
 
   /*  **********************************
   *  Registration Data Validation Rules
@@ -23,15 +25,19 @@ const utilities = require(".")
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
         .trim()
-        .escape()
-        .notEmpty()
         .isEmail()
         .normalizeEmail() // refer to validator.js docs
-        .withMessage("A valid email is required."),
-  
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+          const emailExists = await accountModel.checkExistingEmail(account_email)
+          if (emailExists){
+            throw new Error("Email exists. Please log in or use different email")
+          }
+        }),
+    
       // password is required and must be strong password
       body("account_password")
         .trim()
