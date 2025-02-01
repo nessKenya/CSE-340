757a9754cdd124464a91e888df374ec09130c352
nessKenya <nelson.muchonji@gmail.com>
2025-02-01 23:20:50 +0300

    correct image quotes

--- utilities/index.js
+++ utilities/index.js
@@ -37,7 +37,7 @@ Util.buildClassificationGrid = async function(data){
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_model 
-      +' /></a>'
+      +'" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
       grid += '<h2>'
