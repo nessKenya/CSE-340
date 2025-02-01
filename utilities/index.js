63caf3252b581328ca7e41046cae170b6332ff8d
nessKenya <nelson.muchonji@gmail.com>
2025-02-01 23:31:27 +0300

    changes to alternative text

--- utilities/index.js
+++ utilities/index.js
@@ -36,7 +36,7 @@ Util.buildClassificationGrid = async function(data){
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_model 
+      +'" alt=" '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +'" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
