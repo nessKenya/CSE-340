f44d8808bf172af7d6095f1d7685d6cfeb60b75c
nessKenya <nelson.muchonji@gmail.com>
2025-02-01 18:06:10 +0300

    Changes to meet WAVE standards

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -36,11 +36,13 @@ validate.inventoryRules = () => {
     .trim()
     .escape()
     .notEmpty().withMessage('Make required.')
+    .isLength({ min: 3 })
     .matches(/^[A-Za-z0-9]+$/).withMessage('Make should have no spaces or special characters'),
     body("inv_model")
     .trim()
     .escape()
     .notEmpty().withMessage('Model required.')
+    .isLength({ min: 3 })
     .matches(/^[A-Za-z0-9]+$/).withMessage('Model should have no spaces or special characters'),
     body("inv_year")
     .trim()
